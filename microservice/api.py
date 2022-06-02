12340cc91bfda8f8b2d83c13461a22916e4697f1
Paweł Müller <pawelmuller42@gmail.com>
2022-06-02 18:03:35 +0200

    Initialised microservice

---
+++ microservice/api.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/predict/A")
+async def get_prediction_a(request: Request):
+    result = None
+    return result
+
+
+@app.post("/predict/B")
+async def get_prediction_b(request: Request):
+    result = None
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info", reload=True)
