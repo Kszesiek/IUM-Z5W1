1237fd6098a0032f306c36dd61b9b640def5e33b
Kszesiek <kszesiek@gmail.com>
2022-06-04 19:01:00 +0200

    Model A class - predict & generate_model

--- models/model_a.py
+++ models/model_a.py
@@ -25,7 +25,38 @@ class ModelA(Model):
                 sessions: DataFrame,
                 users: DataFrame):
         # Calculates self.model
-        self.model = None
+
+        sessions = pd.merge(sessions, products, on="product_id").drop(
+            columns=['product_name', 'category_path']).sort_values(by=['timestamp'], ascending=False)
+
+        next_purchase = {user_id: datetime.now() for user_id in users["user_id"]}
+        spending_factors = {user_id: 0 for user_id in users["user_id"]}
+        actual_total = 0
+
+        learning_days = (max(sessions["timestamp"]) - min(sessions["timestamp"])).days
+        last_session_timestamp = sessions["timestamp"].max()
+
+        for session in sessions.iterrows():
+            session = session[1]
+            user_id = session["user_id"]
+            if session["event_type"] == "VIEW_PRODUCT":
+                continue
+            actual_total += session["price"] * (1 - session["offered_discount"] / 100)
+            if next_purchase[user_id]:
+                time_difference = max(next_purchase[user_id] - session["timestamp"], timedelta(hours=24))
+                time_difference = time_difference.total_seconds() / 86400
+                days_from_today = (last_session_timestamp - session["timestamp"]).total_seconds() / 86400 / 30
+                favor_newer_records_with_tanh = (0.5 + 0.5 * math.tanh(- days_from_today / 2 + 2))
+                spending_factors[user_id] += session["price"] * (
+                            1 - session["offered_discount"] / 100) / time_difference * favor_newer_records_with_tanh
+            next_purchase[user_id] = session["timestamp"]
+
+        correction = actual_total / sum(spending_factors.values())
+        spending_factors = {user_id: factor * correction for (user_id, factor) in spending_factors.items()}
+
+        max_factor = max(spending_factors.values())
+        spending_factors_perc = {key: val / max_factor * 100 for (key, val) in spending_factors.items()}
+        self.model = {key: val * 30 / learning_days for (key, val) in spending_factors.items()}
 
 
 deliveries_path = "./data/deliveries.jsonl"
@@ -38,15 +69,15 @@ products_data = pd.read_json(products_path, lines=True)
 sessions_data = pd.read_json(sessions_path, lines=True)
 users_data = pd.read_json(users_path, lines=True)
 
-sessions_data = pd.merge(sessions_data, products_data, on="product_id").drop(
+sessions_modified = pd.merge(sessions_data, products_data, on="product_id").drop(
         columns=['product_name', 'category_path']).sort_values(by=['timestamp'], ascending=False)
 
-sessions_learn = sessions_data
-sessions_validate = sessions_data
+sessions_learn = sessions_modified
+sessions_validate = sessions_modified
 
 sessions_learn = sessions_learn[sessions_learn["timestamp"].dt.month != 5]
 sessions_validate = sessions_validate[sessions_validate["timestamp"].dt.month == 5]
-sessions_validate_period = sessions_validate.max()["timestamp"].day
+sessions_validate_period = sessions_validate["timestamp"].max().day
 
 
 
@@ -112,5 +143,8 @@ def validate(model, sessions_validate):
 
 
 if __name__ == "__main__":
-    model1 = learn(sessions_learn)
-    validate(model1, sessions_validate)
+    modelA = ModelA()
+    modelA.generate_model(products_data, deliveries_data, sessions_data, users_data)
+    print(modelA.predict(products_data, deliveries_data, sessions_data, users_data))
+    # model1 = learn(sessions_learn)
+    # validate(model1, sessions_validate)
