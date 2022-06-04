ec12570e599028cce439b28845eeeb6da40e7c7b
Kszesiek <kszesiek@gmail.com>
2022-06-04 19:08:57 +0200

    got rid of everything not Model-class related

--- models/model_a.py
+++ models/model_a.py
@@ -59,92 +59,17 @@ class ModelA(Model):
         self.model = {key: val * 30 / learning_days for (key, val) in spending_factors.items()}
 
 
-deliveries_path = "./data/deliveries.jsonl"
-products_path = "./data/products.jsonl"
-sessions_path = "./data/sessions.jsonl"
-users_path = "./data/users.jsonl"
-
-deliveries_data = pd.read_json(deliveries_path, lines=True)
-products_data = pd.read_json(products_path, lines=True)
-sessions_data = pd.read_json(sessions_path, lines=True)
-users_data = pd.read_json(users_path, lines=True)
-
-sessions_modified = pd.merge(sessions_data, products_data, on="product_id").drop(
-        columns=['product_name', 'category_path']).sort_values(by=['timestamp'], ascending=False)
-
-sessions_learn = sessions_modified
-sessions_validate = sessions_modified
-
-sessions_learn = sessions_learn[sessions_learn["timestamp"].dt.month != 5]
-sessions_validate = sessions_validate[sessions_validate["timestamp"].dt.month == 5]
-sessions_validate_period = sessions_validate["timestamp"].max().day
-
-
-
-
-def learn(sessions_learn):
-    next_purchase = {user_id: datetime.now() for user_id in users_data["user_id"]}
-    spending_factors = {user_id: 0 for user_id in users_data["user_id"]}
-    actual_total = 0
-
-    learning_days = (max(sessions_learn["timestamp"]) - min(sessions_learn["timestamp"])).days
-
-    for session in sessions_learn.iterrows():
-        session = session[1]
-        user_id = session["user_id"]
-        if session["event_type"] == "VIEW_PRODUCT":
-            continue
-        actual_total += session["price"] * (1 - session["offered_discount"] / 100)
-        if next_purchase[user_id]:
-            time_difference = max(next_purchase[user_id] - session["timestamp"], timedelta(hours=24))
-            time_difference = time_difference.total_seconds() / 86400
-            days_from_today = (datetime.now() - session["timestamp"]).total_seconds() / 86400 / 30
-            favor_newer_records_with_tanh = (0.5 + 0.5 * math.tanh(- days_from_today / 2 + 2))
-            spending_factors[user_id] += session["price"] * (1 - session["offered_discount"] / 100) / time_difference * favor_newer_records_with_tanh
-        next_purchase[user_id] = session["timestamp"]
-
-    correction = actual_total / sum(spending_factors.values())
-    spending_factors = {user_id: factor * correction for (user_id, factor) in spending_factors.items()}
-
-    max_factor = max(spending_factors.values())
-    spending_factors_perc = {key: val / max_factor * 100 for (key, val) in spending_factors.items()}
-    monthly_est = {key: val * sessions_validate_period / learning_days for (key, val) in spending_factors.items()}
-
-    return monthly_est
-
-
-def validate(model, sessions_validate):
-    total_for_user = {user_id: 0 for user_id in users_data["user_id"]}
-
-    for session in sessions_validate.iterrows():
-        session = session[1]
-        user_id = session["user_id"]
-        if session["event_type"] == "VIEW_PRODUCT":
-            continue
-        total_for_user[user_id] += session["price"] * (1 - session["offered_discount"] / 100)
-
-    total = 0
-    sum_ratio = 0
-    for user_id in users_data["user_id"]:
-        if total_for_user[user_id] == 0:
-            ratio = 0 if model[user_id] < 10 else min(model[user_id] / 10, 1)
-        else:
-            ratio = min((model[user_id] - total_for_user[user_id]) / total_for_user[user_id], 1)
-        print(f"user {user_id}: {ratio:3.2f} ({model[user_id]:3.1f}/{total_for_user[user_id]:3.1f})")
-        total += total_for_user[user_id]
-        sum_ratio += abs(ratio)
-    print(f"dokładność: {100 * ( 1 - sum_ratio / len(users_data.index)):6.2f}%")
-    print(f"suma modelu: {sum(model.values()):9.2f}")
-    print(f"suma actual: {total:9.2f}")
-
-    # dokładność:  39.77%
-    # suma modelu: 204590.67
-    # suma actual: 219801.26
-
-
 if __name__ == "__main__":
+    deliveries_path = "./data/deliveries.jsonl"
+    products_path = "./data/products.jsonl"
+    sessions_path = "./data/sessions.jsonl"
+    users_path = "./data/users.jsonl"
+
+    deliveries_data = pd.read_json(deliveries_path, lines=True)
+    products_data = pd.read_json(products_path, lines=True)
+    sessions_data = pd.read_json(sessions_path, lines=True)
+    users_data = pd.read_json(users_path, lines=True)
+
     modelA = ModelA()
     modelA.generate_model(products_data, deliveries_data, sessions_data, users_data)
     print(modelA.predict(products_data, deliveries_data, sessions_data, users_data))
-    # model1 = learn(sessions_learn)
-    # validate(model1, sessions_validate)
--- models/model_b_simplified.py
+++ models/model_b_simplified.py
@@ -50,28 +50,6 @@ class ModelB(Model):
         self.model = clf.fit(fitting_data, y)
 
 
-deliveries_path = "./data/deliveries.jsonl"
-products_path = "./data/products.jsonl"
-sessions_path = "./data/sessions.jsonl"
-users_path = "./data/users.jsonl"
-
-deliveries_data = pd.read_json(deliveries_path, lines=True)
-products_data = pd.read_json(products_path, lines=True)
-sessions_data = pd.read_json(sessions_path, lines=True)
-users_data = pd.read_json(users_path, lines=True)
-
-sessions_modified = pd.merge(sessions_data, products_data, on="product_id")
-sessions_modified = pd.merge(sessions_modified, users_data, on="user_id").sort_values(by=['timestamp'], ascending=True)
-# sessions_modified = pd.merge(sessions_modified, deliveries_data, on="purchase_id")
-
-sessions_learn = sessions_modified
-sessions_validate = sessions_modified
-
-sessions_learn = sessions_learn[sessions_learn["timestamp"].dt.month != 5]
-sessions_validate = sessions_validate[sessions_validate["timestamp"].dt.month == 5]
-sessions_validate_period = sessions_validate["timestamp"].max().day
-
-
 def convert_data_from_dataframe_to_list(dataframe_data):
     data_as_list = []
     dataframe_data = dataframe_data.drop(
@@ -84,10 +62,6 @@ def convert_data_from_dataframe_to_list(dataframe_data):
         data_as_list.append(row)
 
     return data_as_list
-
-
-sessions_learn_list = convert_data_from_dataframe_to_list(sessions_learn)
-sessions_validate_list = convert_data_from_dataframe_to_list(sessions_validate)
 
 
 def prepare_labels(dataframe):
@@ -115,77 +89,17 @@ def prepare_labels(dataframe):
     return labels
 
 
-def learn(learning_data):
-    y = prepare_labels(learning_data)
-
-    fitting_data = convert_data_from_dataframe_to_list(learning_data)
-
-    clf = tree.DecisionTreeRegressor()
-    return clf.fit(fitting_data, y)
-
-
-def validate(model, validating_data):
-    user_predictions = {user_id: 0 for user_id in users_data["user_id"]}
-    predictions_per_user = {user_id: 0 for user_id in users_data["user_id"]}
-
-    for (user_id, prediction) in zip([row[2] for row in validating_data], model.predict(validating_data)):
-        user_predictions[user_id] += prediction
-        predictions_per_user[user_id] += 1
-
-    for user_id, predictions_sum in user_predictions.items():
-        # adjust to validating data - data from may ends on 25th of May, but predictions were made for a full month
-        user_predictions[user_id] = predictions_sum * sessions_validate_period / 30 / (predictions_per_user[user_id] or 1)
-
-    total_for_user = {user_id: 0 for user_id in users_data["user_id"]}
-
-    for session in validating_data:
-        if session[4] == 0:
-            continue
-        user_id = session[2]
-        total_for_user[user_id] += session[6] * (1 - session[5] / 100)
-
-    total = 0
-    sum_ratio = 0
-    for user_id in users_data["user_id"]:
-        if total_for_user[user_id] == 0:
-            ratio = 0 if user_predictions[user_id] < 10 else min(user_predictions[user_id] / 10, 1)
-        else:
-            ratio = min((user_predictions[user_id] - total_for_user[user_id]) / total_for_user[user_id], 1)
-        print(f"user {user_id}: {ratio:3.2f} ({user_predictions[user_id]:3.1f}/{total_for_user[user_id]:3.1f})")
-        total += total_for_user[user_id]
-        sum_ratio += abs(ratio)
-    print(f"dokładność: {100 * (1 - sum_ratio / len(users_data.index)):6.2f}%")
-    print(f"suma modelu: {sum(user_predictions.values()):9.2f}")
-    print(f"suma actual: {total:9.2f}")
-
-    # dokładność: 36.49 %
-    # suma modelu: 247519.09
-    # suma actual: 219801.26
-
-
-def predict(products, deliveries, sessions, users, model):
-    sessions = pd.merge(sessions, products, on="product_id")
-    sessions = pd.merge(sessions, users, on="user_id").sort_values(by=['timestamp'], ascending=True)
-
-    sessions_list = convert_data_from_dataframe_to_list(sessions_data)
-
-    sessions_length = (sessions["timestamp"].max() - sessions["timestamp"].min()).days
-    user_predictions = {user_id: 0 for user_id in users["user_id"]}
-    predictions_per_user = {user_id: 0 for user_id in users["user_id"]}
-
-    for (user_id, prediction) in zip([row[2] for row in sessions_list], model.predict(sessions_list)):
-        user_predictions[user_id] += prediction
-        predictions_per_user[user_id] += 1
-
-    for user_id, predictions_sum in user_predictions.items():
-        # adjust to validating data - data from may ends on 25th of May, but predictions were made for a full month
-        user_predictions[user_id] = predictions_sum / (predictions_per_user[user_id] or 1)
-
-    return user_predictions
-
-
 if __name__ == "__main__":
+    deliveries_path = "./data/deliveries.jsonl"
+    products_path = "./data/products.jsonl"
+    sessions_path = "./data/sessions.jsonl"
+    users_path = "./data/users.jsonl"
+
+    deliveries_data = pd.read_json(deliveries_path, lines=True)
+    products_data = pd.read_json(products_path, lines=True)
+    sessions_data = pd.read_json(sessions_path, lines=True)
+    users_data = pd.read_json(users_path, lines=True)
+
     modelB = ModelB()
     modelB.generate_model(products_data, deliveries_data, sessions_data, users_data)
     print(modelB.predict(products_data, deliveries_data, sessions_data, users_data))
-    # validate(model, sessions_validate_list)
