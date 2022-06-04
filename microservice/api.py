bbfca3d4c9d2ca4de5488f3fad48a158a75c8ca5
Paweł Müller <pawelmuller42@gmail.com>
2022-06-04 16:32:49 +0200

    Added models for incoming data

--- microservice/api.py
+++ microservice/api.py
@@ -1,31 +1,74 @@
+from datetime import datetime
+
 import uvicorn
 from fastapi import FastAPI, Request, Response, status, HTTPException
-
-from models import model_a, model_b
+from pydantic import BaseModel, Field
+from typing import Dict, List
 
 app = FastAPI()
 
 
+class DeliveryModel(BaseModel):
+    purchase_id: int
+    purchase_timestamp: datetime
+    delivery_timestamp: datetime
+    delivery_company: int
+
+
+class ProductModel(BaseModel):
+    product_id: int
+    product_name: str
+    category_path: str
+    price: float = Field(...)
+    brand: str
+    weight_kg: float
+    optional_attributes: Dict[str, str] = Field(...)
+
+
+class SessionModel(BaseModel):
+    session_id: int
+    timestamp: datetime
+    user_id: int
+    product_id: int
+    event_type: str
+    offered_discount: int
+    purchase_id: int = None
+
+
+class UserModel(BaseModel):
+    user_id: int
+    name: str
+    city: str
+    street: str
+
+
+class PredictRequestModel(BaseModel):
+    deliveries: List[DeliveryModel]
+    products: List[ProductModel]
+    sessions: List[SessionModel]
+    users: List[UserModel]
+
+
 @app.post("/predict/A")
-async def get_prediction_a(request: Request, response: Response):
-    data = request
-    result = model_a.predict(data)
+async def get_prediction_a(body: PredictRequestModel, response: Response):
+    # result = model_a.predict(body)
+    result = body
     if result:
         response.status_code = status.HTTP_200_OK
         return result
     else:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="No prediction returned.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No prediction returned.")
 
 
 @app.post("/predict/B")
-async def get_prediction_b(request: Request, response: Response):
-    data = request
-    result = model_b.predict(data)
+async def get_prediction_b(body: PredictRequestModel, response: Response):
+    # result = model_b_simplified.predict(request)
+    result = body
     if result:
         response.status_code = status.HTTP_200_OK
         return result
     else:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="No prediction returned.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No prediction returned.")
 
 
 @app.post("/predict")
@@ -36,4 +79,4 @@ async def get_prediction_ab(request: Request, response: Response):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info", reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8080, log_level="info", reload=True)
